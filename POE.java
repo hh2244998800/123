b7373d1e0e1ea30014179c523a68d29579d1aec1
hh2244998800 <hh2244998800@gmail.com>
2016-03-04 16:34:04 +0800

    七言絕句

---
+++ POE.java
@@ -0,0 +1,12 @@
+
+class seven 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("李益<從軍北征>\n");
+		System.out.println("天山雪後海風寒\n");
+		System.out.println("橫笛偏吹行路難\n");
+		System.out.println("磧里征人三十萬\n");
+		System.out.println("一時回向月明看\n");
+	}
+}
